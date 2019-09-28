96b6bf5b81f373c8895afad4a5952c4398388894
uni51 <uni51@users.noreply.github.com>
2019-09-28 16:10:27 +0900

    9-1-2　テストクラスの作成

---
+++ tests/Unit/CalculatePointServiceTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CalculatePointServiceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * @test
+     */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
