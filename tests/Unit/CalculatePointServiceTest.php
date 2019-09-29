79cdfbbb4c5e1fb6d85c812a8f13922e56271629
uni51 <uni51@users.noreply.github.com>
2019-09-29 11:03:00 +0900

    9-1-4 データプロバイダの活用

--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -8,41 +8,63 @@ use Tests\TestCase;
 
 class CalculatePointServiceTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+//    /**
+//     * A basic test example.
+//     *
+//     * @return void
+//     */
+//    public function testExample()
+//    {
+//        $this->assertTrue(true);
+//    }
+
+//    /**
+//     * @test
+//     */
+//    public function example()
+//    {
+//        $this->assertTrue(true);
+//    }
+
+//    /**
+//     * @test
+//     */
+//    public function calcpoint_購入金額が0ならポイントは0()
+//    {
+//        $result = CalculatePointService::calcPoint(0);
+//
+//        $this->assertSame(0, $result); // ① $resultが0であることを検証
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function calcpoint_購入金額が1000ならポイントは10()
+//    {
+//        $result = CalculatePointService::calcPoint(1000);
+//
+//        $this->assertSame(10, $result); // ① $resultが0であることを検証
+//    }
 
     /**
      * @test
+     * @dataProvider dataProvider_for_calcPoint
      */
-    public function example()
+    public function calcpoint(int $expected, int $amount)
     {
-        $this->assertTrue(true);
+        $result = CalculatePointService::calcPoint($amount);
+
+        $this->assertSame($expected, $result);
     }
 
-    /**
-     * @test
-     */
-    public function calcpoint_購入金額が0ならポイントは0()
+    public function dataProvider_for_calcPoint(): array
     {
-        $result = CalculatePointService::calcPoint(0);
-
-        $this->assertSame(0, $result); // ① $resultが0であることを検証
-    }
-
-    /**
-     * @test
-     */
-    public function calcpoint_購入金額が1000ならポイントは10()
-    {
-        $result = CalculatePointService::calcPoint(1000);
-
-        $this->assertSame(10, $result); // ① $resultが0であることを検証
+        return [
+            '購入金額が0なら0ポイント'       => [0, 0],
+            '購入金額が999なら0ポイント'     => [0, 999],
+            '購入金額が1000なら10ポイント'   => [10, 1000],
+            '購入金額が9999なら99ポイント'   => [99, 9999],
+            '購入金額が10000なら200ポイント' => [200, 10000],
+        ];
     }
 }
